af74cc0ea0f9710bf6cd7cce6d32ac80fb2d20fd
Kris Mally <kristophermally@gmail.com>
2022-06-16 21:35:18 -0600

    Optimize monsters search functionality

--- src/App.js
+++ src/App.js
@@ -18,29 +18,37 @@ componentDidMount() {
       .then(users => this.setState(() => {
         return { monsters: users }
       }))
-  }
+}
 
-  render() {
-    const filteredMonsters = this.state.monsters.filter(monster => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
-    });
-    return (
-      <div className="App">
-        <input type="text" placeholder="Search Monsters" onChange={(event) => {
-            const searchField = event.target.value.toLocaleLowerCase();
-            this.setState(() => {
-              return { searchField };
-            })
-    }
-        }/>
-        {
-          filteredMonsters.map((monster, index) => {
-            return <h1 key={index}>{monster.name}</h1>
-          })
-        }
-      </div>
-    );
-  }
+onSearchChange = event => {
+  const searchField = event.target.value.toLocaleLowerCase();
+  this.setState(() => {
+    return { searchField };
+  })
+}
+
+render() {
+  const { monsters,searchField } = this.state;
+  const { onSearchChange } = this;
+  
+  // Store updating filtered list of monsters in new array based on search field
+  const filteredMonsters = monsters.filter(monster => {
+    return monster.name.toLocaleLowerCase().includes(searchField);
+  });
+  return (
+    <div className="App">
+      <input type="text" placeholder="Search Monsters" onChange={onSearchChange} />
+      {filteredMonsters.map((monster, index) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          )
+        })
+      }
+    </div>
+  );
+}
 }
 
 export default App;

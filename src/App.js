01efaece5b2ec8e98c56217fcb9e7946a0120aa7
Kris Mally <kristophermally@gmail.com>
2022-06-17 20:32:40 -0600

    Create and render card list component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+
+import CardList from './components/card-list/CardList';
 import './App.css';
 
 class App extends Component {
@@ -11,44 +13,52 @@ class App extends Component {
     }
   }
 
-// gets monsters from API
-componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(response => response.json())
-      .then(users => this.setState(() => {
-        return { monsters: users }
-      }))
-}
+  // gets monsters from API
+  componentDidMount() {
+      fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response => response.json())
+        .then(users => this.setState(() => {
+          return { monsters: users }
+        }))
+  }
 
-onSearchChange = event => {
-  const searchField = event.target.value.toLocaleLowerCase();
-  this.setState(() => {
-    return { searchField };
-  })
-}
+  // 
+  onSearchChange = event => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    })
+  }
 
-render() {
-  const { monsters,searchField } = this.state;
-  const { onSearchChange } = this;
-  
-  // Store updating filtered list of monsters in new array based on search field
-  const filteredMonsters = monsters.filter(monster => {
-    return monster.name.toLocaleLowerCase().includes(searchField);
-  });
-  return (
-    <div className="App">
-      <input type="text" placeholder="Search Monsters" onChange={onSearchChange} />
-      {filteredMonsters.map((monster, index) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          )
-        })
-      }
-    </div>
-  );
-}
-}
+  render() {
+    // Destructured
+    const { monsters,searchField } = this.state;
+    const { onSearchChange } = this;
+
+    // Store updating filtered list of monsters in new array based on search field
+    const filteredMonsters = monsters.filter(monster => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    return (
+      <div className="App">
+        <input 
+          className="search-box" 
+          type="text" 
+          placeholder="Search Monsters" 
+          onChange={onSearchChange}
+        />
+         {// filteredMonsters.map(monster => {
+        //     return (
+        //       <div key={monster.id}>
+        //         <h1>{monster.name}</h1>
+        //       </div>
+        //     )
+        //   })
+        }
+        <CardList />
+      </div>
+    )};
+};
 
 export default App;

f15629230e687cbb59481454e268b68a35531098
Kris Mally <kristophermally@gmail.com>
2022-07-07 21:03:10 -0600

    Refactor card list component to functional

--- src/components/card-list/CardList.js
+++ src/components/card-list/CardList.js
@@ -1,21 +1,13 @@
-import { Component } from "react";
-
 import Card from "../card/Card";
 import "./card-list.styles.css";
 
-class CardList extends Component {
-    render() {
-        const { monsters } = this.props;
-
-        return (
-            <div className="card-list">
-                {monsters.map(monster => {
-                    // destructuring
-                    return <Card monster={monster} />
-                })}
-            </div>
-        )
-    }
-}
+const CardList = ({ monsters }) => (
+    <div className="card-list">
+        {monsters.map(monster => {
+            // destructuring
+            return <Card monster={monster} />
+        })}
+    </div>
+);
 
 export default CardList;
